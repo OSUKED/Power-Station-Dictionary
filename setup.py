2201c517e4f9a314dbc51037072e96d4468bc076
AyrtonB <AyrtonBourn@Outlook.com>
2020-12-18 14:28:12 +0000

    added install requirements

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url=setup_kwargs['git_url'],
     packages=setuptools.find_packages(),
+    install_requires=setup_kwargs['requirements']
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
